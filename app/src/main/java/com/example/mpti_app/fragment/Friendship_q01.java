82c8ef47ef147bb756627103ac5c833e3ae279fb
Hongsoojeong <my_youth99@naver.com>
2022-02-16 00:52:01 +0900

    테스트 문항 화면 넘어가기 구현

--- app/src/main/java/com/example/mpti_app/fragment/Friendship_q01.java
+++ app/src/main/java/com/example/mpti_app/fragment/Friendship_q01.java
@@ -1,12 +1,11 @@
 package com.example.mpti_app.fragment;
 
 import android.app.Fragment;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
@@ -16,7 +15,22 @@ public class Friendship_q01 extends Fragment {
 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_friendship_q01, container, false);
- 
+        TextView option1 = (TextView) view.findViewById(R.id.q01_option1Y);
+        TextView option2 = (TextView) view.findViewById(R.id.q01_option1N);
+
+        option1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getFragmentManager().beginTransaction().replace(R.id.mainactivity_framelayout, new Friendship_q02()).commit();
+            }
+        });
+        option2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getFragmentManager().beginTransaction().replace(R.id.mainactivity_framelayout, new Friendship_q02()).commit();
+            }
+        });
+
         return view;
     }
 }
---
+++ app/src/main/java/com/example/mpti_app/fragment/Friendship_q02.java
@@ -0,0 +1,21 @@
+package com.example.mpti_app.fragment;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+
+import com.example.mpti_app.R;
+
+public class Friendship_q02 extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_friendship_q02, container, false);
+        return view;
+    }
+
+}
